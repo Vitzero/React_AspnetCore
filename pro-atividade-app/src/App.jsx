cee9c7907a2e2d5685828ed2d69de48a1f654927
VitorBaldasso <vitor.baldasso05@gmail.com>
2026-02-27 15:50:41 -0300

    first commit

---
+++ pro-atividade-app/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  const atividades = [
+    {
+      id: 0,
+      descricao: 'Primeira atividade',
+    },
+  ];
+
+  return(
+    <div className='mt-3'>
+     
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+
+    </div>
+  );
+}
+
+  
+
+export default App
