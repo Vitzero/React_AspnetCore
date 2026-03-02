5c4116a1520dad7dd7858a3ce7df7556087cd77e
VitorBaldasso <vitor.baldasso05@gmail.com>
2026-03-02 16:31:04 -0300

    feito useState

--- pro-atividade-app/src/App.jsx
+++ pro-atividade-app/src/App.jsx
@@ -1,38 +1,58 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const atividades = [
-    {
-      id: 0,
-      descricao: 'Primeira atividade',
-    },
-    {
-      id: 1,
-      descricao: 'segunda atividade',
-    },
-    {
-      id: 2,
-      descricao: 'terceira atividade',
-    },
-  ];
+  const [atividades, setAtividades] = useState([
+    { id: 1, descricao: "seila bixo" },
+    { id: 2, descricao: "seila bixo 2" },
+  ]);
 
-  return(
-    <div className='mt-3'>
-      <form>
-        <input id="id" type="text" placeholder='id'></input>
-        <input id="descricao" type="text" placeholder='informe a descrição'></input>
-        <button > Add </button>
+  const [id, setId] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  function AddAtividade(e) {
+    e.preventDefault();
+
+    const atividade = {
+      id,
+      descricao,
+    };
+
+    setAtividades(prev => [...prev, atividade]);
+
+    setId("");
+    setDescricao("");
+  }
+
+  return (
+    <div className="mt-3">
+      <form onSubmit={AddAtividade}>
+        <input
+          type="text"
+          placeholder="id"
+          value={id}
+          onChange={e => setId(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="informe a descrição"
+          value={descricao}
+          onChange={e => setDescricao(e.target.value)}
+        />
+
+        <button type="submit">Add</button>
       </form>
-      <ul class="list-group">
-        {atividades.map( x => (
-          <li key={x.id} class="list-group-item">{x.id} {x.descricao}</li>
+
+      <ul className="list-group">
+        {atividades.map(x => (
+          <li key={x.id} className="list-group-item">
+            {x.id} {x.descricao}
+          </li>
         ))}
       </ul>
-
     </div>
   );
 }
 
-  
-
-export default App
+export default App;
