51684bb45880d06ff3a28d271b1395fdf1958247
VitorBaldasso <vitor.baldasso05@gmail.com>
2026-03-03 16:25:46 -0300

    botoes create e delete adicionados e criado novas labels e inputs

--- pro-atividade-app/src/App.jsx
+++ pro-atividade-app/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [atividades, setAtividades] = useState([
@@ -9,6 +9,8 @@ function App() {
 
   const [id, setId] = useState("");
   const [descricao, setDescricao] = useState("");
+  const [titulo, setTitulo] = useState("");
+  const [prioridade, setPrioridade] = useState("");
 
   function AddAtividade(e) {
     e.preventDefault();
@@ -16,41 +18,118 @@ function App() {
     const atividade = {
       id,
       descricao,
+      titulo,
+      prioridade
     };
 
-    setAtividades(prev => [...prev, atividade]);
+    setAtividades((prev) => [...prev, atividade]);
 
     setId("");
     setDescricao("");
+    setTitulo("");
   }
 
   return (
-    <div className="mt-3">
-      <form onSubmit={AddAtividade}>
-        <input
-          type="text"
-          placeholder="id"
-          value={id}
-          onChange={e => setId(e.target.value)}
-        />
+    <div class="col-md-6 flex">
+      <form class="row g-3 ">
+        <div class="col-md-6">
+          <label className="form-label" type="text" placeholder="id">
+            ID
+          </label>
 
-        <input
-          type="text"
-          placeholder="informe a descrição"
-          value={descricao}
-          onChange={e => setDescricao(e.target.value)}
-        />
 
-        <button type="submit">Add</button>
+          
+          <input
+            className="form-control"
+            type="text"
+            placeholder="id"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        </div>
+
+        <div class="col-md-6">
+           <label for="inputState" class="form-label">State</label>
+            <select id="inputState" class="form-select">
+                <option defaultValue="0">Selecionar...</option>
+                <option value="1">Baixa</option>
+                <option value="2">Normal</option>
+                <option value="3">Alta</option>
+            </select>
+        </div>
+
+        <div class="col-md-6">
+          <label className="form-label" type="text" placeholder="titulo">
+            Titulo
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="titulo"
+            value={titulo}
+            onChange={(e) => setTitulo(e.target.value)}
+          />
+        </div>
+        
+        <div class="col-md-6">
+          <label className="form-label" type="text" placeholder="descricao">
+            Descrição
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="descricao"
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+          />
+        </div>
+
+        <div class="col-12">
+          <button
+            onClick={AddAtividade}
+            className="btn btn-outline-secondary"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
       </form>
 
-      <ul className="list-group">
-        {atividades.map(x => (
-          <li key={x.id} className="list-group-item">
-            {x.id} {x.descricao}
-          </li>
+      <div className="mt-3">
+        {atividades.map((x) => (
+          <div className="card mb-2 shadow-sm" key={x.id} class="card">
+            
+            <div class="card-body">
+              
+              <div className="d-flex justify-content-between">
+                <h5 className="card-title">
+                <span className="badge bg-ssecondary me-1">
+                  {x.id}
+                </span>
+                  - {x.titulo}
+                </h5>
+                <h6> Prioridade: Normal </h6>
+              </div>
+              
+              <p class="card-text">
+                {x.descricao}
+              </p>
+              
+              <div className="d-flex justify-content-end pt-2 border-top">
+                
+                <button className="btn btn-outline-primary">
+                  Editar
+                </button>
+                
+                <button className="btn btn-outline-danger">
+                  Deletar
+                </button>
+
+              </div>
+            </div>
+          </div> 
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
