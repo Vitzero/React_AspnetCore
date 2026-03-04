208372bc12a954cd389c18dba6cc1127758e5a46
VitorBaldasso <vitor.baldasso05@gmail.com>
2026-03-04 16:53:37 -0300

    feito ids sequenciais e pintando border line

--- pro-atividade-app/src/App.jsx
+++ pro-atividade-app/src/App.jsx
@@ -3,62 +3,78 @@ import "./App.css";
 
 function App() {
   const [atividades, setAtividades] = useState([
-    { id: 1, descricao: "seila bixo" },
-    { id: 2, descricao: "seila bixo 2" },
+    { id: 1, titulo: "testando", descricao: "seila bixo", prioridade: 1 },
+    { id: 2, titulo: "testando", descricao: "seila bixo 2", prioridade: 2 },
   ]);
 
-  const [id, setId] = useState("");
   const [descricao, setDescricao] = useState("");
   const [titulo, setTitulo] = useState("");
-  const [prioridade, setPrioridade] = useState("");
+  const [prioridade, setPrioridade] = useState(0);
 
-  function AddAtividade(e) {
-    e.preventDefault();
-
-    const atividade = {
-      id,
-      descricao,
-      titulo,
-      prioridade
-    };
-
-    setAtividades((prev) => [...prev, atividade]);
-
-    setId("");
-    setDescricao("");
-    setTitulo("");
+  function PrioridadeLabel(param) {
+    switch (param) {
+      case 1:
+        return "Baixa";
+      case 2:
+        return "Normal";
+      case 3:
+        return "Alta";
+      default:
+        return "Não Definido";
+    }
   }
 
+  function prioridadeStyle(param) {
+  switch (param) {
+    case 1:
+      return "success";   // verde
+    case 2:
+      return "warning";   // amarelo
+    case 3:
+      return "danger";    // vermelho
+    default:
+      return "secondary";
+  }
+}
+
+ function AddAtividade(e) {
+  e.preventDefault();
+
+  const novoId =
+    atividades.length > 0
+      ? Math.max(...atividades.map((item) => item.id)) + 1
+      : 1;
+
+  const atividade = {
+    id: novoId,
+    descricao,
+    titulo,
+    prioridade,
+  };
+
+  setAtividades((prev) => [...prev, atividade]);
+
+  setDescricao("");
+  setTitulo("");
+  setPrioridade(0);
+}
   return (
-    <div class="col-md-6 flex">
-      <form class="row g-3 ">
-        <div class="col-md-6">
-          <label className="form-label" type="text" placeholder="id">
-            ID
+    <div className="col-md-6 flex">
+      <form className="row g-3 ">
+
+        <div className="col-md-6">
+          <label className="form-label">
+            State
           </label>
-
-
-          
-          <input
-            className="form-control"
-            type="text"
-            placeholder="id"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
+          <select id="inputState" className="form-select" value={prioridade} onChange={(e) => setPrioridade(Number(e.target.value))}>
+            <option defaultValue="0">Selecionar...</option>
+            <option value="1">Baixa</option>
+            <option value="2">Normal</option>
+            <option value="3">Alta</option>
+          </select>
         </div>
 
-        <div class="col-md-6">
-           <label for="inputState" class="form-label">State</label>
-            <select id="inputState" class="form-select">
-                <option defaultValue="0">Selecionar...</option>
-                <option value="1">Baixa</option>
-                <option value="2">Normal</option>
-                <option value="3">Alta</option>
-            </select>
-        </div>
-
-        <div class="col-md-6">
+        <div className="col-md-6">
           <label className="form-label" type="text" placeholder="titulo">
             Titulo
           </label>
@@ -70,8 +86,8 @@ function App() {
             onChange={(e) => setTitulo(e.target.value)}
           />
         </div>
-        
-        <div class="col-md-6">
+
+        <div className="col-md-6">
           <label className="form-label" type="text" placeholder="descricao">
             Descrição
           </label>
@@ -84,7 +100,7 @@ function App() {
           />
         </div>
 
-        <div class="col-12">
+        <div className="col-12">
           <button
             onClick={AddAtividade}
             className="btn btn-outline-secondary"
@@ -97,37 +113,31 @@ function App() {
 
       <div className="mt-3">
         {atividades.map((x) => (
-          <div className="card mb-2 shadow-sm" key={x.id} class="card">
-            
-            <div class="card-body">
-              
+          <div className={'card mb-2 shadow-sm border-' + prioridadeStyle(x.prioridade)} key={x.id}>
+            <div className="card-body">
               <div className="d-flex justify-content-between">
                 <h5 className="card-title">
-                <span className="badge bg-ssecondary me-1">
-                  {x.id}
-                </span>
-                  - {x.titulo}
+                  <span className="badge bg-secondary me-1">{x.id}</span>-{" "}
+                  {x.titulo}
                 </h5>
-                <h6> Prioridade: Normal </h6>
+                <h6> 
+                  Prioridade: 
+                    <span className={"ms-1 text-" + prioridadeStyle(x.prioridade)}>
+                      <i className="me-1 far fa-frown color-"></i>
+                      {PrioridadeLabel(x.prioridade)}
+                    </span>
+                  </h6>
               </div>
-              
-              <p class="card-text">
-                {x.descricao}
-              </p>
-              
-              <div className="d-flex justify-content-end pt-2 border-top">
-                
-                <button className="btn btn-outline-primary">
-                  Editar
-                </button>
-                
-                <button className="btn btn-outline-danger">
-                  Deletar
-                </button>
 
+              <p className="card-text">{x.descricao}</p>
+
+              <div className="d-flex justify-content-end pt-2 border-top">
+                <button className="btn btn-outline-primary">Editar</button>
+
+                <button className="btn btn-outline-danger">Deletar</button>
               </div>
             </div>
-          </div> 
+          </div>
         ))}
       </div>
     </div>
